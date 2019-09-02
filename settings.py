2c817b6da7641e894cedc2da2ccdb84c5201245c
huangZchao <604217824@qq.com>
2019-09-02 21:45:32 +0800

    paper version

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 
-flags.DEFINE_list('hidden3', [64, 32, 16], 'Number of units in hidden layer 3.')
+flags.DEFINE_list('hidden3', [64, 32, 16], 'Number of units in TCN.')
 flags.DEFINE_integer('discriminator_out', 0, 'discriminator_out.')
 flags.DEFINE_float('discriminator_learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_float('learning_rate', .5*0.001, 'Initial learning rate.')
@@ -13,10 +13,10 @@ flags.DEFINE_float('weight_decay', 0., 'Weight for L2 loss on embedding matrix.'
 flags.DEFINE_float('dropout', 0., 'Dropout rate (1 - keep probability).')
 flags.DEFINE_integer('features', 0, 'Whether to use features (1) or not (0).')
 flags.DEFINE_integer('seed', 50, 'seed for fixing the results.')
-flags.DEFINE_integer('iterations', 100, 'number of iterations.')
+flags.DEFINE_integer('iterations', 1000, 'number of iterations.')
 # flags.DEFINE_integer('seq_len', 6, 'time stamp for each train.')
-flags.DEFINE_float('time_decay', 0.3, 'decay for each step.')
-flags.DEFINE_float('alpha', 1, 'contribution of structural loss.')
+flags.DEFINE_float('time_decay', 0.1, 'decay for each step.')
+flags.DEFINE_float('alpha', 10, 'contribution of structural loss.')
 
 
 def get_settings(dataname, seq_len):
